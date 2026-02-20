f6e31b2a388058d6fb829b5709c17681e49005fb
Hua Gong <gonghuabupt@gmail.com>
2026-02-20 12:08:52 -0800

    build: increment versionCode to 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.tomandy.oneclaw"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
+        versionCode = 8
         versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
